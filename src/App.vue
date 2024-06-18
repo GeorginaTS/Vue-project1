0d394aeaaaca1b7f25b349e6e04e40d4c88049cd
Georgina <georginatomass@gmail.com>
2024-06-18 10:46:23 +0200

    v0

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-3xl w-full text-center font-extrabold p-4 text-emerald-600">{{ msg }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return{
+      msg: "Hello world"
+    }
+  }
+  
+}
+</script>
